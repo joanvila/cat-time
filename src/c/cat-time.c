4fec20bf47408ca85e1048459c01a1430fb1f1dc
joanvila <vila.joan94@gmail.com>
2016-10-18 22:24:07 +0200

    Checking the weather every 30 minutes

--- src/c/cat-time.c
+++ src/c/cat-time.c
@@ -47,6 +47,14 @@ static void update_time() {
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
     update_time();
+
+    // Get the weather every 30 minutes
+    if (tick_time->tm_min % 30 == 0) {
+        DictionaryIterator *iter;
+        app_message_outbox_begin(&iter);
+        dict_write_uint8(iter, 0, 0);
+        app_message_outbox_send();
+    }
 }
 
 static void prv_window_load(Window *window) {
